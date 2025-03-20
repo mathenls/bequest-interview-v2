b249709bce020ea4cdbe3fc5a25a2cc670364800
Matheus Lucas <math.enlds@gmail.com>
2025-03-20 17:51:44 -0300

    feat: backend and main repo changes

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -8,7 +8,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: true, // Allow requests from any origin in development
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    },
+  });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
